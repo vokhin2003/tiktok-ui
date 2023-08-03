b99f18794d661b0b8fa579138bd817ecdadbdf49
Khin Vo <khinvo2003@gmail.com>
2023-08-03 17:51:44 +0700

    Header #1

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/e6d725cb6a17bbe02e6e60704c1305a8~c5_100x100.jpeg?x-expires=1691046000&x-signature=BNC6ZHA44e9BTvVN026edqfO4g8%3D"
+                alt="Hoaa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')} >nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
